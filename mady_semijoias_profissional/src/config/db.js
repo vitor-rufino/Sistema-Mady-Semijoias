9ab48881cca0b3afb9ae136d3e5e298265709a55
Vitor Rufino <jvm.rufino@gmail.com>
2026-06-22 18:21:19 -0400

    Adicionar DB_PORT

--- mady_semijoias_profissional/src/config/db.js
+++ mady_semijoias_profissional/src/config/db.js
@@ -2,6 +2,9 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
+  const pool = mysql.createPool({
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "mady_semijoias",
