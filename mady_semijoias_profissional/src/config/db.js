b6b63a673e3e544c6f17ab2dafe58c664a868e3e
Vitor Rufino <jvm.rufino@gmail.com>
2026-06-22 18:29:22 -0400

    Corrigir db.js

--- mady_semijoias_profissional/src/config/db.js
+++ mady_semijoias_profissional/src/config/db.js
@@ -2,8 +2,6 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
-  const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
