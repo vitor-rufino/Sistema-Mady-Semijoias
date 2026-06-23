181615fe4f26e7b50fd27cf87233c4ed1dda0cf7
Vitor Rufino <jvm.rufino@gmail.com>
2026-06-23 18:25:02 -0400

    Corrigir carrinho

--- mady_semijoias_profissional/src/routes/userRoutes.js
+++ mady_semijoias_profissional/src/routes/userRoutes.js
@@ -4,6 +4,8 @@ const userController = require("../controllers/userController");
 const { isClient } = require("../middlewares/auth");
 
 router.get("/", isClient, userController.dashboard);
+router.get("/carrinho", isClient, userController.cart);
+router.post("/carrinho/remover/:id", isClient, userController.removeFromCart);
 router.post("/produtos/:id/adicionar", isClient, userController.addProductToAccount);
 router.get("/minha-conta", isClient, userController.myAccount);
 
